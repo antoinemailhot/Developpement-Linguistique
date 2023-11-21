f3fceafefb71ef8d5dabeee7880a7eecabbc0f29
Antoine Mailhot <capitaineam@outlook.com>
2023-11-21 12:21:13 -0500

    Mise à jour main + lecture du fichier english.csv

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Import de fichier.
+import csv
 
+# Main
 def main():
-
-
+    with open('./EnglishHandwrittenCharacters/english.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(', '.join(row))
 
 
 if __name__ == "__main__":
