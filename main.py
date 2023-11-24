77fe8cbcde13fd3788a3e80db827ee2b726f099d
Antoine Mailhot <capitaineam@outlook.com>
2023-11-24 17:45:56 -0500

    Ajout mesure distance manmhanttan et ajout mesure distance euclidienne

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ def main():
 
 # Calcul distance 
 
-# Calcul distance hamming sur deux vecteur.
+# Calcul distance hamming sur deux vecteur. //Cours INF1183-SE-09-data_minning.pdf p.21
 def distance_Hamming(vecteur1, vecteur2):
    return np.sum(vecteur1 != vecteur2)
 
+# Calcul distance //Cours INF1183-SE-09-data_minning.pdf p.20
+
+# Mesure distance manhattan. (p = 1) //INF1183-SE-09-data_minning.pdf p.20
+def mesure_distance_manhattan(vecteur1, vecteur2):
+   return np.sum(np.abs(vecteur1 - vecteur2))
+
+# Mesure distance euclidienne. (p = 2) //INF1183-SE-09-data_minning.pdf p.20
+def mesure_distance_euclidienne(vecteur1, vecteur2):
+   return np.sqrt(np.sum(vecteur1 - vecteur2) ** 2)
 
 # Fin calcul distance
 
